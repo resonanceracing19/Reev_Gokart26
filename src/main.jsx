16cc95a90622327aeb3a59faaeb742a1c52fa35b
resonanceracing19 <resonanceracing19@gmail.com>
2026-04-11 01:00:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./App.css";
 import App from "./App.jsx";
-import Stairs from "./Components/St/Stairs.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Stairs>
+      
         <App />
-      </Stairs>
+      
     </BrowserRouter>
   </StrictMode>
 );
